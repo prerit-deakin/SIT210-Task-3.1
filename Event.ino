8633be85fc931156a2d3826b71ec1b4b045e2488
prerit-deakin <preritjaiswal65@gmail.com>
2025-05-18 00:48:11 +1000

    Update Event.ino

--- Event.ino
+++ Event.ino
@@ -4,45 +4,48 @@
 #include <BH1750.h>
 #include <IFTTTMaker.h>
 
-char ssid[] = "Prerit's Pixel 7 Pro";       
-char password[] = "Prerit123";  
-#define KEY "gJ_G5ib4jvxmQDljur-X_Jrhq0UYNEYkJ9SkPpo_j3s"  
-#define EVENT_NAME "sunlight_on" 
-
-
-
+char ssid[] = "Prerit's Pixel 7 Pro";
+char password[] = "Prerit14";
+#define KEY "gJ_G5ib4jvxmQDljur-X_Jrhq0UYNEYkJ9SkPpo_j3s"
+#define EVENT_NAME "sunlight_on"
 
 WiFiSSLClient client;
 IFTTTMaker ifttt(KEY, client);
+BH1750 lightMeter;
 
 void setup() {
-
   Serial.begin(115200);
 
-  // Attempt to connect to Wifi network:
-  Serial.print("Connecting Wifi: ");
-  Serial.println(ssid);
+  Serial.print("Connecting to WiFi: ");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+
+  Serial.println("\nWiFi connected!");
   Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
+  Serial.println(WiFi.localIP());
 
-
-  //triggerEvent takes an Event Name and then you can optional pass in up to 3 extra Strings
-  if(ifttt.triggerEvent(EVENT_NAME, ssid)){
-    Serial.println("Successfully sent");
-  } else
-  {
-    Serial.println("Failed!");
-  }
-
+  Wire.begin();
+  lightMeter.begin();
+  Serial.println("BH1750 sensor initialized.");
 }
 
 void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light level: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+
+  if (lux > 50) {
+    if (ifttt.triggerEvent(EVENT_NAME)) {
+      Serial.println("IFTTT trigger sent!");
+    } else {
+      Serial.println("IFTTT trigger failed.");
+    }
+    delay(600000); // wait 10 minutes before triggering again
+  }
+
+  delay(5000); // check light every 5 seconds
 }
